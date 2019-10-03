f00bc9805cac3efb6888d556fa74190643733867
slipknotts <55863113+slipknotts@users.noreply.github.com>
2019-10-03 16:52:05 -0700

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 yum -y install httpd mod_ssl
 systemctl start httpd
-vi /etc/httpd/conf.d/welcome.conf
 sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf
 echo "<html><body><h1>Hola NTI 300</h1></body></html>" > /var/www/html/index.html
